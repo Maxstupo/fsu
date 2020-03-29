f97996691306eab26fff441f71268c61257ba59e
Maxstupo <maxstupo@hotmail.com>
2020-03-29 23:53:06 +1100

    Add project skeleton.

---
+++ fsu.core/Class1.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Maxstupo.Fsu.Core {
+
+    public class Class1 {
+
+
+
+    }
+
+}
---
+++ fsu/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Maxstupo.Fsu.Core;
+
+namespace Maxstupo.Fsu {
+
+    public class Program {
+
+        public static void Main(string[] args) {
+
+        }
+
+    }
+
+}
