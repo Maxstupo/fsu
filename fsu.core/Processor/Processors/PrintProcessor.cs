1020c7b9ad03984b3c6ae5e1b9c32fc642ed9a69
Maxstupo <maxstupo@hotmail.com>
2020-07-01 15:36:53 +1000

    Support changing print column separator.

--- fsu.core/Processor/Processors/PrintProcessor.cs
+++ fsu.core/Processor/Processors/PrintProcessor.cs
@@ -1,5 +1,6 @@
 ﻿namespace Maxstupo.Fsu.Core.Processor.Processors {
-
+   
+    using System;
     using System.Collections.Generic;
     using System.Linq;
     using Maxstupo.Fsu.Core.Processor;
@@ -8,17 +9,15 @@
 
     public class PrintProcessor : IProcessor {
 
-        private readonly bool useTable = true;
-        private readonly char separator = '|';
+        private readonly string[] separator;
 
-        public PrintProcessor(bool useTable, char separator) {
-            this.useTable = useTable;
-            this.separator = separator;
+        public PrintProcessor(string separator = null) {
+            this.separator = separator == null ? null : new string[] { separator };
         }
 
         public IEnumerable<ProcessorItem> Process(IProcessorPipeline pipeline, IEnumerable<ProcessorItem> items) {
 
-            if (useTable) {
+            if (separator != null) {
 
                 ConsoleTable table = new ConsoleTable {
                     Printer = new ColorConsoleTablePrinter(pipeline.Console)
@@ -27,7 +26,7 @@
 
                 string value = items.FirstOrDefault()?.Value ?? null;
                 if (value != null) {
-                    int columnCount = value.Split(separator).Length;
+                    int columnCount = value.Split(separator, StringSplitOptions.RemoveEmptyEntries).Length;
                     for (int y = 0; y < columnCount; y++)
                         table.Columns.Add(string.Empty);
                 } else {
@@ -39,7 +38,7 @@
                 string[] columns = new string[1];
                 foreach (ProcessorItem item in items) {
                     columns[0] = (i++).ToString();
-                    table.Rows.Add(columns.Concat(item.Value.Split(separator)).ToArray());
+                    table.Rows.Add(columns.Concat(item.Value.Split(separator, StringSplitOptions.RemoveEmptyEntries)).ToArray());
                 }
                 table.WriteRows(false);
 
