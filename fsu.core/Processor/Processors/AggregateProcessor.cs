7d61f00b7a29a975f33bc4769f6fb7ce528d2729
Maxstupo <maxstupo@hotmail.com>
2020-07-06 22:09:24 +1000

    Add aggregate processor base class.

---
+++ fsu.core/Processor/Processors/AggregateProcessor.cs
@@ -0,0 +1,45 @@
+﻿namespace Maxstupo.Fsu.Core.Processor.Processors {
+
+    using System;
+    using System.Collections.Generic;
+    using Maxstupo.Fsu.Core.Detail;
+
+    public abstract class AggregateProcessor : IProcessor {
+
+        protected abstract string Key { get; }
+
+        private readonly string itemPropertyName;
+        private readonly string outputPropertyName;
+
+        public AggregateProcessor(string itemPropertyName, string outputPropertyName = null) {
+            this.itemPropertyName = itemPropertyName ?? throw new ArgumentNullException(nameof(itemPropertyName));
+            this.outputPropertyName = outputPropertyName;
+        }
+
+        protected abstract PropertyItem CalculateAggregation(IProcessorPipeline pipeline, IEnumerable<ProcessorItem> items);
+
+        public IEnumerable<ProcessorItem> Process(IProcessorPipeline pipeline, IEnumerable<ProcessorItem> items) {
+
+            PropertyItem result = CalculateAggregation(pipeline, items);
+
+            if (result != null) {
+                string gPropertyName = outputPropertyName ?? $"{Key}_{itemPropertyName}";
+
+                pipeline.PropertyStore.SetProperty(gPropertyName, result);
+            }
+
+            return items;
+        }
+
+        protected PropertyItem GetProperty(IProcessorPipeline pipeline, ProcessorItem item) {
+            return item.GetProperty(pipeline.PropertyProvider, itemPropertyName);
+        }
+
+        public override string ToString() {
+            return $"{GetType().Name}[key={Key}, itemPropertyName={itemPropertyName}, outputPropertyName={outputPropertyName}]";
+        }
+
+
+    }
+
+}
--- fsu.core/Processor/Processors/AvgProcessor.cs
+++ fsu.core/Processor/Processors/AvgProcessor.cs
@@ -5,32 +5,29 @@
     using Maxstupo.Fsu.Core.Detail;
     using Maxstupo.Fsu.Core.Utility;
 
-    public sealed class AvgProcessor : IProcessor {
+    public sealed class AvgProcessor : AggregateProcessor {
 
-        private readonly string itemPropertyName;
-        private readonly string outputPropertyName;
+        protected override string Key => "avg";
 
-        public AvgProcessor(string itemPropertyName, string outputPropertyName = null) {
-            this.itemPropertyName = itemPropertyName ?? throw new ArgumentNullException(nameof(itemPropertyName));
-            this.outputPropertyName = outputPropertyName;
+        public AvgProcessor(string itemPropertyName, string outputPropertyName = null) : base(itemPropertyName, outputPropertyName) {
+
         }
 
-        public IEnumerable<ProcessorItem> Process(IProcessorPipeline pipeline, IEnumerable<ProcessorItem> items) {
-
+        protected override PropertyItem CalculateAggregation(IProcessorPipeline pipeline, IEnumerable<ProcessorItem> items) {
             double total = 0;
             int count = 0;
 
             Enum unit = null;
 
             foreach (ProcessorItem item in items) {
-                PropertyItem property = item.GetProperty(pipeline.PropertyProvider, itemPropertyName);
+                PropertyItem property = GetProperty(pipeline, item);
 
                 if (property == null)
                     continue;
 
                 if (!property.IsNumeric) {
                     pipeline.Output.WriteLine(Level.Warn, "The avg processor can only average numeric properties!");
-                    return items;
+                    return null;
                 }
 
                 total += property.ValueNumber;
@@ -41,16 +38,9 @@
 
             double average = total / count;
 
-            string gPropertyName = outputPropertyName ?? $"avg_{itemPropertyName}";
-
-            pipeline.PropertyStore.SetProperty(gPropertyName, new PropertyItem(average, unit));
-
-            return items;
+            return new PropertyItem(average, unit);
         }
 
-        public override string ToString() {
-            return $"{GetType().Name}[itemPropertyName={itemPropertyName}, outputPropertyName={outputPropertyName}]";
-        }
 
     }
 
