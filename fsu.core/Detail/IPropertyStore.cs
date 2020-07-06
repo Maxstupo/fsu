2d4a806cc0035b71b9a360e05ba1e862731a6382
Maxstupo <maxstupo@hotmail.com>
2020-07-06 22:08:34 +1000

    Add persistent global properties.

--- fsu.core/Detail/IPropertyStore.cs
+++ fsu.core/Detail/IPropertyStore.cs
@@ -3,11 +3,13 @@
     public interface IPropertyStore {
 
         void Clear();
+        
+        void ClearAll();
 
-        void SetProperty(string propertyName, PropertyItem property);
+        void SetProperty(string propertyName, PropertyItem property, bool persistent = false);
 
         PropertyItem GetProperty(string propertyName);
-
+      
     }
 
 }
--- fsu.core/Detail/PropertyStore.cs
+++ fsu.core/Detail/PropertyStore.cs
@@ -2,25 +2,35 @@
 
     using System;
     using System.Collections.Generic;
+    using System.Linq;
 
     public class PropertyStore : IPropertyStore {
 
-        private readonly Dictionary<string, PropertyItem> items = new Dictionary<string, PropertyItem>(StringComparer.InvariantCultureIgnoreCase);
+        private readonly Dictionary<string, object[]> items = new Dictionary<string, object[]>(StringComparer.InvariantCultureIgnoreCase);
 
         public void Clear() {
+            List<string> keysToRemove = items.Where(x => !(bool) x.Value[1]).Select(x => x.Key).ToList();
+            foreach (string key in keysToRemove)
+                items.Remove(key);
+        }
+
+        public void ClearAll() {
             items.Clear();
         }
 
         public PropertyItem GetProperty(string propertyName) {
-            return items.TryGetValue(propertyName, out PropertyItem item) ? item : null;
+            return items.TryGetValue(propertyName, out object[] item) ? (PropertyItem) item[0] : null;
         }
 
-        public void SetProperty(string propertyName, PropertyItem property) {
+        public void SetProperty(string propertyName, PropertyItem property, bool persistent = false) {
 
             if (items.ContainsKey(propertyName))
                 return;
 
-            items.Add(propertyName, property);
+            items.Add(propertyName, new object[] {
+               property,
+               persistent
+            });
 
         }
 
