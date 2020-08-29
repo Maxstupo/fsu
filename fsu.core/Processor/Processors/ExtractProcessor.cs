78a079c5d4c41fbade4373b87913a99df87d43f9
Maxstupo <maxstupo@hotmail.com>
2020-08-29 15:14:52 +1000

    Use numeric properties when possible.

--- fsu.core/Processor/Processors/ExtractProcessor.cs
+++ fsu.core/Processor/Processors/ExtractProcessor.cs
@@ -10,17 +10,17 @@
 
     public class ExtractProcessor : IProcessor {
 
+        private readonly string regexText;
         private readonly string key;
         private readonly string property;
-        private readonly string regexText;
 
         private readonly Regex regex;
 
 
-        public ExtractProcessor(string key, string property, string regex) {
+        public ExtractProcessor(string regex, string key, string property) {
+            this.regexText = regex ?? throw new ArgumentNullException(nameof(regex));
             this.key = key ?? throw new ArgumentNullException(nameof(key));
             this.property = property ?? throw new ArgumentNullException(nameof(property));
-            this.regexText = regex ?? throw new ArgumentNullException(nameof(regex));
 
             this.regex = new Regex(regex, RegexOptions.CultureInvariant | RegexOptions.IgnoreCase);
         }
@@ -30,26 +30,27 @@
                 string propertyValue = item.GetProperty(pipeline.PropertyProvider, property).ValueText;
 
                 Match match = regex.Match(propertyValue);
+                if (match.Success) {
 
+                    item.TryCachePropertyValue(key, CreatePropertyItem(match.Value));
 
-                if (!match.Success)
-                    continue;
+                    if (match.Groups.Count > 1) {
+                        for (int i = 1; i < match.Groups.Count; i++) {
+                            Group group = match.Groups[i];
+                            if (!group.Success)
+                                continue;
 
-
-                item.TryCachePropertyValue(key, new PropertyItem(match.Value));
-
-                if (match.Groups.Count > 1) {
-                    for (int i = 1; i < match.Groups.Count; i++) {
-                        Group group = match.Groups[i];
-                        if (!group.Success)
-                            continue;
-                        item.TryCachePropertyValue($"{key}{i - 1}", new PropertyItem(group.Value));
+                            item.TryCachePropertyValue($"{key}{i - 1}", CreatePropertyItem(group.Value));
+                        }
                     }
                 }
 
                 yield return item;
             }
+        }
 
+        private PropertyItem CreatePropertyItem(string value) {
+            return double.TryParse(value, out double numericValue) ? new PropertyItem(numericValue, null) : new PropertyItem(value);
         }
 
         public override string ToString() {
