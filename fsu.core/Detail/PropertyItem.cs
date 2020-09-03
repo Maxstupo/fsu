b7bf89c07f029ba4791a4e9bf9e678a87e1cc87f
Maxstupo <maxstupo@hotmail.com>
2020-09-03 23:06:56 +1000

    Implement class.

--- fsu.core/Detail/PropertyItem.cs
+++ fsu.core/Detail/PropertyItem.cs
@@ -1,6 +1,46 @@
 ﻿namespace Maxstupo.Fsu.Core.Detail {
 
-    public class PropertyItem {
+    using System;
+
+    /// <summary>
+    /// A metadata value of a processor item. Supports both text and numeric values (optionally a numeric unit for conversions).
+    /// </summary>
+    public sealed class PropertyItem {
+
+        /// <summary>The raw property item value, could be a string or double.</summary>
+        public object Value { get; }
+
+        /// <summary>The value in string form.</summary>
+        public string ValueText => (string) Value;
+
+        /// <summary>The value in number form. Returns zero if property item isn't numeric.</summary>
+        public double ValueNumber => IsNumeric ? Convert.ToDouble(Value) : 0;
+
+        /// <summary>True if this property item represents a number.</summary>
+        public bool IsNumeric { get; }
+
+        /// <summary>An optional enum containing the numeric unit the value is in, used for conversions (e.g. GB to KB).</summary>
+        public Enum Unit { get; }
+
+        /// <summary>
+        /// Constructs a new string PropertyItem.
+        /// </summary>
+        public PropertyItem(string value) {
+            Value = value ?? throw new ArgumentNullException(nameof(value));
+            IsNumeric = false;
+        }
+
+        /// <summary>
+        /// Constructs a new number PropertyItem.
+        /// </summary>
+        public PropertyItem(double value, Enum unit = null) {
+            Value = value;
+            Unit = unit;
+            IsNumeric = true;
+        }
+
+        public static implicit operator string(PropertyItem propertyItem) => propertyItem.ValueText;
+        public static implicit operator double(PropertyItem propertyItem) => propertyItem.ValueNumber;
 
     }
 
--- fsu.core/Processor/ProcessorItem.cs
+++ fsu.core/Processor/ProcessorItem.cs
@@ -1,8 +1,67 @@
 ﻿namespace Maxstupo.Fsu.Core.Processor {
 
-    public class ProcessorItem {
+    using System;
+    using System.Collections.Generic;
+    using Maxstupo.Fsu.Core.Detail;
 
+    /// <summary>
+    /// Represents an item passed between processors. Each item represents something as a string, this could be a filepath, url, or anything.
+    /// ProcessItems remember their inital value, and provide the means to cache properties related to this processor item.
+    /// </summary>
+    public sealed class ProcessorItem {
 
+        /// <summary>The initial readonly value of this processor item.</summary>
+        public string InitialValue { get; }
+
+        private string value;
+        /// <summary>The current value of this processor item. Modifiying this value will result in the clearing of all previously cached properties.</summary>
+        public string Value {
+            get => value;
+            set {
+                if (this.value != value)
+                    this.cachedProperties.Clear();
+                this.value = value;
+            }
+        }
+
+        /// <summary>A cache of property items previously obtained.</summary>
+        private readonly Dictionary<string, PropertyItem> cachedProperties = new Dictionary<string, PropertyItem>(StringComparer.InvariantCultureIgnoreCase);
+
+        /// <summary>
+        /// Constructs a new ProcessorItem.
+        /// </summary>
+        /// <param name="origin">An optional value that will set a property value called "origin". Used as a "source" for where this processor item was obtained form.</param>
+        public ProcessorItem(string value, string origin = null) {
+            Value = value ?? throw new ArgumentNullException(nameof(value));
+            InitialValue = value;
+
+            if (origin != null)
+                TryCachePropertyValue("origin", new PropertyItem(origin));
+        }
+
+        /// <summary>
+        /// Returns a property item obtained from the property provider given.        
+        /// Properties will be cached within this processor item, attempts to get the same property item will result in the cached version being returned.
+        /// </summary>
+        /// <param name="propertyProvider">The provider to use as the source of property item lookups.</param>
+        /// <param name="propertyName">The name of the property to obtain.</param>
+        public PropertyItem GetProperty(IPropertyProvider propertyProvider, string propertyName) {
+
+            if (cachedProperties.TryGetValue(propertyName, out PropertyItem property)) {
+                return property;
+            } else {
+                property = propertyProvider.GetProperty(propertyProvider, this, propertyName.ToLowerInvariant());
+
+                TryCachePropertyValue(propertyName, property);
+
+                return property;
+            }
+        }
+
+        public void TryCachePropertyValue(string propertyName, PropertyItem property) {
+            if (propertyName != null && property != null && !cachedProperties.ContainsKey(propertyName))
+                cachedProperties.Add(propertyName, property);
+        }
 
     }
 
